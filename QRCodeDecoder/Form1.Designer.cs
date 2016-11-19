727be149fbc59c1d1774f0be07e637ad00cc77ca
Ayush Bansal <ayushb666@gmail.com>
2016-11-19 04:06:08 -0600

    Added Numeric Encoding

---
+++ QRCodeDecoder/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace QRCodeDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectPic = new System.Windows.Forms.LinkLabel();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.message = new System.Windows.Forms.Label();
+            this.decoder = new System.Windows.Forms.Button();
+            this.qrImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.qrImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectPic
+            // 
+            this.selectPic.AutoSize = true;
+            this.selectPic.Location = new System.Drawing.Point(12, 9);
+            this.selectPic.Name = "selectPic";
+            this.selectPic.Size = new System.Drawing.Size(103, 20);
+            this.selectPic.TabIndex = 0;
+            this.selectPic.TabStop = true;
+            this.selectPic.Text = "Select Image";
+            this.selectPic.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "openFileDialog1";
+            this.fileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.fileDialog_FileOk);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(12, 526);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(74, 20);
+            this.message.TabIndex = 3;
+            this.message.Text = "Message";
+            // 
+            // decoder
+            // 
+            this.decoder.Location = new System.Drawing.Point(460, 215);
+            this.decoder.Name = "decoder";
+            this.decoder.Size = new System.Drawing.Size(253, 89);
+            this.decoder.TabIndex = 4;
+            this.decoder.Text = "Decode";
+            this.decoder.UseVisualStyleBackColor = true;
+            this.decoder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // qrImage
+            // 
+            this.qrImage.Location = new System.Drawing.Point(16, 46);
+            this.qrImage.Name = "qrImage";
+            this.qrImage.Size = new System.Drawing.Size(438, 447);
+            this.qrImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.qrImage.TabIndex = 6;
+            this.qrImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 577);
+            this.Controls.Add(this.qrImage);
+            this.Controls.Add(this.decoder);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.selectPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.qrImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel selectPic;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Button decoder;
+        private System.Windows.Forms.PictureBox qrImage;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+
+    }
+}
+
---
+++ QRCodeDecoder/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeDecoder
+{
+    public partial class Form1 : Form
+    {
+        private string fileName = "";
+        private int n = 21;
+        private int[,] dataMatrix = new int[21, 21];
+        private int[,] permanentMatrix = new int[21, 21];
+        private string maskpattern = "";
+        private string errorpattern = "";
+        private string encodingType = "";
+        private int messageLength;
+        string encodedMessage;
+        private int lengthEachBlock;
+
+        private delegate int Mydelegate(int i, int j);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            qrImage.Visible = false;
+            decoder.Enabled = false;
+            message.Text = "";
+            fillPermanentMatrix();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            fileDialog.FileName = "";
+            fileDialog.Title = "Select a QR Image";
+            fileDialog.Filter = "All images|*.jpg; *.bmp; *.png";
+            fileDialog.ShowDialog();
+            if (fileDialog.FileName.ToString() != "")
+            {
+                fileName = fileDialog.FileName.ToString();
+                selectPic.Text = fileName;
+                qrImage.Image = Image.FromFile(fileName);
+                qrImage.Visible = true;
+                decoder.Enabled = true;
+                message.Text = "File Uploaded";
+            }
+        }
+
+        // Currently works for a 21*21 size qr code and Byte Encoded Message.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap dataFile = new Bitmap(fileName);
+            Bitmap data = getSizeofQR(data: dataFile);
+            qrImage.Image = Image.FromHbitmap(data.GetHbitmap());
+            getDataMatrix(data: data);
+            getUnmaskedInfoBits();
+            unmaskDataMatrix();
+            getEncodingTypeandLength();
+            getEncodedData(); // the data is in reverse order here
+            message.Text = "Your Message is : " + convertByteMessage();
+            decoder.Enabled = false;
+        }
+
+        private string convertByteMessage()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < messageLength * 8; i += 8)
+            {
+                sb.Append(Convert.ToChar(Convert.ToInt32(encodedMessage.Substring(i, 8), 2)));
+            }
+            return sb.ToString();
+        }
+
+        private void getEncodedData()
+        {
+            bool flag = true; // when true moves up and when false move down
+            int temp = messageLength*8;
+            int i = n-7 ,j = n-1;
+            StringBuilder sb = new StringBuilder();
+            while (temp!= 0)
+            {
+                if(permanentMatrix[i,j] == 5 || permanentMatrix[i, j] == 4)
+                {
+                    while(permanentMatrix[i, j] == 5 || permanentMatrix[i, j] == 4)
+                    {
+                        if (flag) { i--; }
+                        else { i++; }
+                    }
+                }
+
+                sb.Append(dataMatrix[i, j]);
+                sb.Append(dataMatrix[i, j - 1]);
+
+                if (flag)
+                {
+                    if ((i - 1 >= 0) && (permanentMatrix[i - 1, j] != 5)){ i--; }
+                    else{
+                        j -= 2;
+                        flag = false;}
+                }else
+                {
+                    if((i+1<=n-1) && (permanentMatrix[i + 1, j] != 5)) { i++; }
+                    else{
+                        j -= 2;
+                        flag = true;}
+                }
+                temp-= 2;
+            }
+            encodedMessage = sb.ToString();
+        }
+
+        private Bitmap getSizeofQR(Bitmap data)
+        {
+            Bitmap temp;
+            int lengthBlackPart = 0, starti = 0, startj = 0, flag1 = 0, flag2 = 0;
+            for (int i = 0; i < data.Height; i++)
+            {
+                for (int j = 0; j < data.Width; j++)
+                {
+                    var t = data.GetPixel(i, j);
+                    if ((t.B+t.G+t.R)/3 <10 && flag1 ==0)
+                    {
+                        if (flag2 == 0)
+                        {
+                            starti = i;
+                            startj = j;
+                            flag2 = 1;
+                        }
+                        lengthBlackPart++;
+                    }else if (lengthBlackPart > 0)
+                    {
+                        flag1 = 1;
+                        break;
+                    }
+                }
+                if(flag1 == 1) { break; }
+            }
+            lengthEachBlock = lengthBlackPart / 7;
+            temp = new Bitmap(lengthEachBlock * n, lengthEachBlock * n);
+            for (int i = 0; i < lengthEachBlock * n; i++)
+            {
+                for (int j = 0; j < lengthEachBlock * n; j++)
+                {
+                    temp.SetPixel(i, j, data.GetPixel(i+starti, j+startj));
+                }
+            }
+            return temp;
+        }
+
+        private void getEncodingTypeandLength()
+        {
+            encodingType  += dataMatrix[n-1,n-1].ToString() + dataMatrix[n-1,n-2].ToString() + dataMatrix[n-2,n-1].ToString() + dataMatrix[n-2,n-2].ToString();
+            string length = dataMatrix[n - 3, n - 1].ToString() + dataMatrix[n - 3, n - 2].ToString() + dataMatrix[n - 4, n - 1].ToString() + dataMatrix[n - 4, n - 2].ToString() + dataMatrix[n - 5, n - 1].ToString() + dataMatrix[n - 5, n - 2].ToString() + dataMatrix[n -6, n - 1].ToString() + dataMatrix[n - 6, n - 2].ToString();
+            messageLength = Convert.ToInt32(length,2);
+        }
+
+        private void unmaskDataMatrix()
+        {
+            Mydelegate f;
+
+            switch (maskpattern)
+            {
+                case "000":
+                    f = (i, j) => { return (i + j) % 2; }; 
+                    break;
+
+                case "001":
+                    f = (i, j) => { return i % 2; };
+                    break;
+
+                case "010":
+                    f = (i, j) => { return j % 3; };
+                    break;
+
+                case "011":
+                    f = (i, j) => { return (i + j) % 3; };
+                    break;
+
+                case "100":
+                    f = (i, j) => { return (i / 2 + j / 3) % 2; };
+                    break;
+
+                case "101":
+                    f = (i, j) => { return ((i * j) % 2 + (i * j) % 3); };
+                    break;
+
+                case "110":
+                    f = (i, j) => { return ((i * j) % 3 + (i * j)) % 2; };
+                    break;
+
+                case "111":
+                    f = (i, j) => { return ((i * j) % 3 + i + j) % 2; };
+                    break;
+
+                default:
+                    f = (i, j) => { return (i + j) % 2; };
+                    break;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (f(i,j) == 0 && (permanentMatrix[i,j] !=5 && permanentMatrix[i, j] != 4))
+                    {
+                        int t = dataMatrix[i, j];
+                        if (dataMatrix[i, j] == 0) { dataMatrix[i, j] = 1; }
+                        else if(dataMatrix[i, j] == 1) { dataMatrix[i, j] = 0; }
+                        t = dataMatrix[i, j];
+                    } 
+                }
+            }
+        }
+
+        private void getUnmaskedInfoBits()
+        {
+            string maskpatternTemp = dataMatrix[8, 2].ToString() + dataMatrix[8, 3].ToString() + dataMatrix[8, 4].ToString();
+            string errorpatternTemp = dataMatrix[8, 0].ToString() + dataMatrix[8, 1].ToString();
+            int x = Convert.ToInt32(errorpatternTemp+ maskpatternTemp, 2);
+            x = x ^ Convert.ToInt32("10101",2);
+            string pattern = Convert.ToString(x, 2);
+            if(pattern.Length < 5)
+            {
+                for (int i = 0; i < 5 - pattern.Length; i++)
+                {
+                    pattern = '0' + pattern;
+                }
+            }
+            maskpattern += pattern.Substring(2,3);
+            errorpattern += pattern.Substring(0, 2);
+        }
+
+        private void getDataMatrix(Bitmap data)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var k = data.GetPixel(j*lengthEachBlock+1 ,i*lengthEachBlock+1);
+                    if (((k.R + k.B + k.G)/3) < 100) { dataMatrix[i, j] = 1; }
+                    else { dataMatrix[i, j] = 0; }
+                }
+            }
+        }
+
+        private void fillPermanentMatrix()
+        {   // top left
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = 0; j <= 8; j++)
+                { permanentMatrix[i, j] = 5; } // 5 represents permanent effect
+            }
+            // top right
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = n - 1; j >= n - 8; j--)
+                { permanentMatrix[i, j] = 5; }
+            }
+            // bottom left
+            for (int i = n - 1; i >= n - 8; i--)
+            {
+                for (int j = 0; j <= 8; j++)
+                { permanentMatrix[i, j] = 5; }
+            }
+
+            for (int i = 6; i < n - 7; i += 2) { permanentMatrix[6, i] = 4; }
+            for (int i = 6; i < n - 7; i += 2) { permanentMatrix[i, 6] = 4; }
+
+        }
+
+        private void fileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e){}
+
+    }
+}
